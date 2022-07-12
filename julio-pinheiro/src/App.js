bd7a7aaf16f5d495cf5259cfa5db483bebf1b334
Julio Pinheiro <juliorenan@live.com>
2022-07-11 21:50:28 -0300

    chore: added santander experience

--- julio-pinheiro/src/App.js
+++ julio-pinheiro/src/App.js
@@ -48,123 +48,181 @@ function App() {
 
       <div class="nes-container  with-title">
         <p class="title">Experience</p>
-        <p>Senior System Analyst at Safra Bank (Jan/2021 until now)</p>
-        <div class="lists">
-          <ul class="nes-list is-disc">
-            <li>
-              {" "}
-              <p>
+
+        <div class="nes-container">
+          <p>Senior System Analyst at Safra Bank (Jan/2021 until now)</p>
+
+          <div class="lists">
+            <ul class="nes-list is-disc">
+              <li>
                 {" "}
-                I am responsible for leading a team of 8 people, raising
-                activities to be executed, planning quality deliveries and
-                developing software projects for Banco Safra's Financial Area
-              </p>
-              {"   "}
-            </li>
+                <p>
+                  {" "}
+                  I am responsible for leading a team of 8 people, raising
+                  activities to be executed, planning quality deliveries and
+                  developing software projects for Banco Safra's Financial Area
+                </p>
+                {"   "}
+              </li>
 
-            <li>
-              {" "}
-              <p>
+              <li>
                 {" "}
-                Responsible for developing and gathering requirements for the
-                implementation of Open Banking at Safra Bank, resulting in more
-                than 15,000 customers sharing their information monthly.{" "}
-              </p>
-            </li>
+                <p>
+                  {" "}
+                  Responsible for developing and gathering requirements for the
+                  implementation of Open Banking at Safra Bank, resulting in
+                  more than 15,000 customers sharing their information monthly.{" "}
+                </p>
+              </li>
 
-            <li>
-              {" "}
-              <p>
+              <li>
                 {" "}
-                Development of Safra Bank's new car sales system, bringing more
-                than 1 million active customers and profits of more than 2
-                billion R$ per year.
-              </p>
-            </li>
+                <p>
+                  {" "}
+                  Development of Safra Bank's new car sales system, bringing
+                  more than 1 million active customers and profits of more than
+                  2 billion R$ per year.
+                </p>
+              </li>
 
-            <li>
-              {" "}
-              <p>
-                I led a project to develop a system for simulating and
-                contracting financing for solar panels.{" "}
-              </p>
-            </li>
-
-            <li>
-              <p>
-                I was able to improve the team's performance by 30% by creating
-                documentation and components that can be reused during the
-                software development process.{" "}
-              </p>
-            </li>
-
-            <li>
-              <p>
+              <li>
                 {" "}
-                I was able to decrease the time invested in meetings by 40%
-                using the agile SCRUM methodology.{" "}
-              </p>
-            </li>
+                <p>
+                  I led a project to develop a system for simulating and
+                  contracting financing for solar panels.{" "}
+                </p>
+              </li>
 
-            <li>
-              Currently I am responsible for the search and correction of
-              information security vulnerabilities in all the systems used by
-              Banco Safra's Financial Department. Throughout the work we managed
-              to reduce the number of vulnerabilities by 90%.{" "}
-            </li>
-          </ul>
-          <ul>
-            <p> Main Techs:</p>
+              <li>
+                <p>
+                  I was able to improve the team's performance by 30% by
+                  creating documentation and components that can be reused
+                  during the software development process.{" "}
+                </p>
+              </li>
 
-            <div class="nes-badge">
-              <span class="is-dark">.NET Core</span>
-            </div>
+              <li>
+                <p>
+                  {" "}
+                  I was able to decrease the time invested in meetings by 40%
+                  using the agile SCRUM methodology.{" "}
+                </p>
+              </li>
 
-            <div class="nes-badge">
-              <span class="is-primary">Typescript</span>
-            </div>
-
-            <div class="nes-badge ">
-              <span class="is-dark">Angular</span>
-            </div>
-
-            <div class="nes-badge">
-              <span class="is-primary">Javascript</span>
-            </div>
-
-            <div class="nes-badge ">
-              <span class="is-dark">TailwindCss</span>
-            </div>
-
-            <div class="nes-badge">
-              <span class="is-success">Nodejs</span>
-            </div>
-
-            <div class="nes-badge ">
-              <span class="is-error">Oracle</span>
-            </div>
-
-            <div class="nes-badge">
-              <span class="is-dark">CheckMarx</span>
-            </div>
-
-            <div class="nes-badge ">
-              <span class="is-primary">Kibana</span>
-            </div>
-
-            <div class="nes-badge ">
-              <span class="is-dark">Jenkins</span>
-            </div>
-          </ul>
+              <li>
+                Currently I am responsible for the search and correction of
+                information security vulnerabilities in all the systems used by
+                Banco Safra's Financial Department. Throughout the work we
+                managed to reduce the number of vulnerabilities by 90%.{" "}
+              </li>
+            </ul>
+            <ul>
+              <p> Main Techs:</p>
+              <div class="nes-badge">
+                <span class="is-dark">.NET Core</span>
+              </div>
+              <div class="nes-badge">
+                <span class="is-primary">Typescript</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-dark">Angular</span>
+              </div>
+              <div class="nes-badge">
+                <span class="is-primary">Javascript</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-dark">TailwindCss</span>
+              </div>
+              <div class="nes-badge">
+                <span class="is-success">Nodejs</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-error">Oracle</span>
+              </div>
+              <div class="nes-badge">
+                <span class="is-dark">CheckMarx</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-primary">Kibana</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-dark">Jenkins</span>
+              </div>
+            </ul>
+          </div>
         </div>
 
-        <p>Development Consultant (May/2017 - Jan/2021)</p>
-        <div class="lists">
-          <ul class="nes-list is-disc">
-            <li></li>
+        <div class="nes-container">
+          <p>Development Consultant (May/2017 - Jan/2021)</p>
+          <div class="lists">
+            <ul class="nes-list is-disc">
+              <li>
+                <p>
+                  I developed more than twenty-three business intelligence
+                  panels with management information such as credit card sales,
+                  investments, and customer portfolio management for Santander
+                  Bank commercial managers, resulting in more than 13 million
+                  monthly active customers.
+                </p>
+              </li>
+              <li>
+                <p>
+                  I was also responsible for leading a team of nine people in
+                  the development and maintenance of Santander Bank systems.
+                </p>
+              </li>
+              <li>
+                <p>
+                  {" "}
+                  I developed a new customer prospecting system for Santander
+                  Bank that is used by more than 20 thousand managers, bringing
+                  an average of 15 thousand new customers monthly.
+                </p>
+              </li>
+              <li>
+                <p>
+                  {" "}
+                  I was also responsible for the idealization and creation of a
+                  “Knowledge Academy”, we held technology courses and various
+                  subjects for more than 1500 people at Santander Bank.
+                </p>
+              </li>
+            </ul>
 
-            <li></li>
-          </ul>
+            <ul>
+              <p> Main Techs:</p>
+              <div class="nes-badge">
+                <span class="is-dark">.NET Framework</span>
+              </div>
+              <div class="nes-badge">
+                <span class="is-primary">Oracle</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-dark">Angular</span>
+              </div>
+              <div class="nes-badge">
+                <span class="is-primary">Javascript</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-dark">MVC5</span>
+              </div>
+              <div class="nes-badge">
+                <span class="is-success">Nodejs</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-error">EXT Net MVC</span>
+              </div>
+              <div class="nes-badge">
+                <span class="is-dark">Dynatrace</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-primary">Git</span>
+              </div>
+              <div class="nes-badge ">
+                <span class="is-dark">Jenkins</span>
+              </div>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
